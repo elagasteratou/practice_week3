5f65b4d7a06ffefb4eaeb623312e95484a8e1a68
Asia <asiasharif12@icloud.com>
2021-02-12 14:42:05 +0000

    exercise 10 question 2 complete

--- ex10_starter.py
+++ ex10_starter.py
@@ -7,7 +7,7 @@ else:
     hdir = os.environ['HOME']
 
 # Construct a portable wildcard pattern
-pattern = os.path.join(hdir,'*')
+pattern = os.path.join(hdir, '*')
 
 # TODO: Use the glob.glob() function to obtain the list of filenames
 
@@ -18,3 +18,12 @@ pattern = os.path.join(hdir,'*')
 # TODO: Remove the leading directory name(s) from each filename before you print it - 
 # using os.path.basename()
 
+supplied_pin = input("enter your pin")
+correct_pin = '1234'
+print(type(supplied_pin))
+if supplied_pin == correct_pin:
+    print("correct")
+else:
+    for i in range(0, 2):
+        supplied_pin = input("enter your pin")
+        print("incorrect")
