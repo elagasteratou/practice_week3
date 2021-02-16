8b3cb00f6c0effc89ac4ad68c7e0a299bd78377d
Michaela Gasteratou <elagasteratou@gmail.com>
2021-02-16 13:21:51 +0000

    completed ex10 working on sys.platform

--- ex10_starter.py
+++ ex10_starter.py
@@ -8,15 +8,25 @@ else:
 
 # Construct a portable wildcard pattern
 pattern = os.path.join(hdir, '*')
-
+print("pattern", pattern)
 # TODO: Use the glob.glob() function to obtain the list of filenames
-
+list_files = glob.glob(pattern)
+# print(list_files)
 # TODO: use os.path.getsize to find each file's size
+# for i in list_files:
+#     size_of_file = os.path.getsize(i)
+#     print(size_of_file)
 
 # TODO: Add a test to only display files that are not zero length
+# for i in list_files:
+#     size_of_file = os.path.getsize(i)
+#     if size_of_file != 0:
+#         print(i, size_of_file)
 
 # TODO: Remove the leading directory name(s) from each filename before you print it - 
 # using os.path.basename()
+print("original",list_files[0] )
+print("basename for pattern", os.path.basename(list_files[0]))
 
 supplied_pin = input("enter your pin")
 correct_pin = '1234'
